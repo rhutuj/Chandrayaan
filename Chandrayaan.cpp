50811079de4671355ea2acd6a8bd1d9229350f26
rhutuj <rhutujbhoir@gmail.com>
2023-09-15 11:23:23 +0530

    solved errors for test 1

--- Chandrayaan.cpp
+++ Chandrayaan.cpp
@@ -2,10 +2,25 @@
 
 using namespace std;
 
+class sCraft
+{
+    private:
+    int x,y,z;
+    char direction;
+
+    public:
+    sCraft(int x0, int y0, int z0, char dir0)
+    {
+        x = x0;
+        y = y0;
+        z = z0;
+        direction = dir0;
+    }
+};
 int main()
 {
     int xStart = 0, yStart = 0, zStart = 0;
-    int dirStart  = 'N'
+    int dirStart  = 'N';
     sCraft chandrayan(xStart, yStart, zStart, dirStart);
     
 
